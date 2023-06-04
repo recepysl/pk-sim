ec5761f605b8426e4a8ea79f651cf4aeefa7ca96
Recep <recepysl@gmail.com>
2023-06-04 09:34:39 +0300

    logger and MWL

--- SimApi/SimApi/Middleware/RequestLoggingMiddleware.cs
+++ SimApi/SimApi/Middleware/RequestLoggingMiddleware.cs
@@ -1,70 +1,104 @@
-﻿using Serilog;
+﻿using Microsoft.IO;
 using SimApi.Base;
-using System.Net;
 
 namespace SimApi.Service.Middleware;
 
 public class RequestLoggingMiddleware
 {
     private readonly RequestDelegate next;
-    public RequestLoggingMiddleware(RequestDelegate next)
+    private readonly RecyclableMemoryStreamManager recyclableMemoryStreamManager;
+    private readonly Action<RequestProfilerModel> requestResponseHandler;
+    private const int ReadChunkBufferLength = 4096;
+    public RequestLoggingMiddleware(RequestDelegate next,Action<RequestProfilerModel> requestResponseHandler)
     {
         this.next = next;
+        this.requestResponseHandler = requestResponseHandler;
+        this.recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
     }
 
     public async Task Invoke(HttpContext context)
     {
-        try
+        var model = new RequestProfilerModel
         {
-            Log.Debug($"" +
-                $"MiddleWareLogger init Method: ${context.Request.Method} || " +
-                $"Path: ${context.Request.Path}");
+            RequestTime = new DateTimeOffset(),
+            Context = context,
+            Request = await FormatRequest(context)
+        };
+
+        Stream originalBody = context.Response.Body;
+
+        using (MemoryStream newResponseBody = recyclableMemoryStreamManager.GetStream())
+        {
+            context.Response.Body = newResponseBody;
 
             await next(context);
 
-        }
-        catch (Exception ex)
+            newResponseBody.Seek(0, SeekOrigin.Begin);
+            await newResponseBody.CopyToAsync(originalBody);
+
+            newResponseBody.Seek(0, SeekOrigin.Begin);
+            model.Response = FormatResponse(context, newResponseBody);
+            model.ResponseTime = new DateTimeOffset();
+            requestResponseHandler(model);
+        }    
+    }
+
+    private string FormatResponse(HttpContext context, MemoryStream newResponseBody)
+    {
+        HttpRequest request = context.Request;
+        HttpResponse response = context.Response;
+
+        return $"Http Response Information: {Environment.NewLine}" +
+                $"Schema:{request.Scheme} {Environment.NewLine}" +
+                $"Host: {request.Host} {Environment.NewLine}" +
+                $"Path: {request.Path} {Environment.NewLine}" +
+                $"QueryString: {request.QueryString} {Environment.NewLine}" +
+                $"StatusCode: {response.StatusCode} {Environment.NewLine}" +
+                $"Response Body: {ReadStreamInChunks(newResponseBody)}";
+    }
+
+    private async Task<string> FormatRequest(HttpContext context)
+    {
+        HttpRequest request = context.Request;
+
+        return $"Http Request Information: {Environment.NewLine}" +
+                    $"Schema:{request.Scheme} {Environment.NewLine}" +
+                    $"Host: {request.Host} {Environment.NewLine}" +
+                    $"Path: {request.Path} {Environment.NewLine}" +
+                    $"QueryString: {request.QueryString} {Environment.NewLine}" +
+                    $"Request Body: {await GetRequestBody(request)}";
+    }
+    public async Task<string> GetRequestBody(HttpRequest request)
+    {
+        request.EnableBuffering();
+        using (var requestStream = recyclableMemoryStreamManager.GetStream())
         {
-            Log.Fatal(
-                       $"MiddleWareLogger Path={context.Request.Path} || " +
-                       $"Method={context.Request.Method} || " +
-                       $"Exception={ex.Message}"
-                       );
-
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(new ApiResponse("Internal Server Error.").ToString());
+            await request.Body.CopyToAsync(requestStream);
+            request.Body.Seek(0, SeekOrigin.Begin);
+            return ReadStreamInChunks(requestStream);
         }
-        finally
+    }
+
+    private static string ReadStreamInChunks(Stream stream)
+    {
+        stream.Seek(0, SeekOrigin.Begin);
+        string result;
+        using (var textWriter = new StringWriter())
+        using (var reader = new StreamReader(stream))
         {
-            var ResponseText = "";
-            var RequestText = "";
+            var readChunk = new char[ReadChunkBufferLength];
+            int readChunkLength;
 
-            using (var reader = new MemoryStream())
+            do
             {
-                context.Response.Body = reader;
-                var sr = new StreamReader(context.Response.Body);
-                context.Response.Body.Seek(0, SeekOrigin.Begin);
-                ResponseText = await sr.ReadToEndAsync();
-                context.Response.Body.Seek(0, SeekOrigin.Begin);
-            }
+                readChunkLength = reader.ReadBlock(readChunk, 0, ReadChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            } while (readChunkLength > 0);
 
-            using (var reader = new MemoryStream())
-            {
-                context.Request.Body = reader;
-                var sr = new StreamReader(context.Request.Body);
-                context.Request.Body.Seek(0, SeekOrigin.Begin);
-                RequestText = await sr.ReadToEndAsync();
-                context.Request.Body.Seek(0, SeekOrigin.Begin);
-            }
-
-            Log.Fatal(
-                      $"MiddleWareLogger Path={context.Request.Path} || " +
-                      $"Method={context.Request.Method} || " +
-                      $"Request={RequestText} || " +
-                      $"Response={ResponseText}"
-                      );
+            result = textWriter.ToString();
         }
+
+        return result;
     }
 
 
--- SimApi/SimApi/Startup.cs
+++ SimApi/SimApi/Startup.cs
@@ -5,6 +5,7 @@ using SimApi.Data.Uow;
 using SimApi.Service.Middleware;
 using SimApi.Service.RestExtension;
 using System.Net;
+using static SimApi.Service.Middleware.RequestLoggingMiddleware;
 
 namespace SimApi.Service;
 
@@ -58,7 +59,15 @@ public class Startup
 
         app.UseMiddleware<HeartBeatMiddleware>();
         app.UseMiddleware<ErrorHandlerMiddleware>();
-        app.UseMiddleware<RequestLoggingMiddleware>();
+        Action<RequestProfilerModel> requestResponseHandler = requestProfilerModel =>
+        {
+            Log.Information("-------------Request-Begin------------");
+            Log.Information(requestProfilerModel.Request);
+            Log.Information(Environment.NewLine);
+            Log.Information(requestProfilerModel.Response);
+            Log.Information("-------------Request-End------------");
+        };
+        app.UseMiddleware<RequestLoggingMiddleware>(requestResponseHandler);
 
         app.UseHttpsRedirection();
 
